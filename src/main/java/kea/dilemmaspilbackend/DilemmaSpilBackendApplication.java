bb151a1bdc6d13b79d970e7e274bfb7ab7efedbf
danish <arjacokaki@hotmail.com>
2022-10-25 15:19:40 +0200

    Project setup

---
+++ src/main/java/kea/dilemmaspilbackend/DilemmaSpilBackendApplication.java
@@ -0,0 +1,13 @@
+package kea.dilemmaspilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilemmaSpilBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DilemmaSpilBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dilemmaspilbackend/DilemmaSpilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dilemmaspilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilemmaSpilBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
