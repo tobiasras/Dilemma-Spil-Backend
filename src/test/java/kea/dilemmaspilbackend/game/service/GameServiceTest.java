e0c6324290d47014c0cac6408d8163709e9801af
danish <arjacokaki@hotmail.com>
2022-11-23 10:20:45 +0100

    Wrote unit tests

--- src/test/java/kea/dilemmaspilbackend/game/service/GameServiceTest.java
+++ src/test/java/kea/dilemmaspilbackend/game/service/GameServiceTest.java
@@ -1,6 +1,5 @@
 package kea.dilemmaspilbackend.game.service;
 
-import kea.dilemmaspilbackend.game.model.GameLobby;
 import kea.dilemmaspilbackend.game.model.Player;
 import kea.dilemmaspilbackend.game.repository.GameRepository;
 import org.junit.jupiter.api.Assertions;
@@ -18,10 +17,49 @@ public class GameServiceTest {
     }
 
     @Test
-    public void testCreateLobby(){
+    public void testCreateGameLobby(){
         // Tests if a lobby is added to the game lobby list by checking if the lobby code works as a key in the list.
         Player player = new Player();
         String lobbyCode = gameService.createGameLobby(player);
         Assertions.assertNotNull(gameRepository.getGameLobbyList().get(lobbyCode));
+    }
+
+    @Test
+    public void testJoinGameLobby(){
+        // Tests a player joining a game lobby. If the player exists in the player list of the lobby, the test is successful
+        Player player = new Player();
+        player.setName("Thomas");
+        String lobbyCode = gameService.createGameLobby(player);
+        gameService.joinGameLobby(player, lobbyCode);
+        Assertions.assertEquals(gameRepository.getGameLobbyList().get(lobbyCode).getPlayerList().get(0).getName(), player.getName());
+    }
+
+    @Test
+    public void testRemoveGameLobby(){
+        // Tests if a lobby is removed from the list of active games. If the lobby is removed, the test is successful.
+        Player player = new Player();
+        String lobbyCode = gameService.createGameLobby(player);
+        gameService.removeGameLobby(lobbyCode);
+        Assertions.assertNull(gameRepository.getGameLobbyList().get(lobbyCode));
+    }
+
+    @Test
+    public void testLeaveGameLobby(){
+        // Tests if a player has left a game lobby. If the player leaves, it's a success
+        Player player = new Player();
+        player.setName("Thomas");
+        String lobbyCode = gameService.createGameLobby(player);
+        gameService.leaveGameLobby(player, lobbyCode);
+        Assertions.assertTrue(gameRepository.getGameLobbyList().get(lobbyCode).getPlayerList().isEmpty());
+    }
+
+    @Test
+    public void testReadyUp(){
+        // Tests if a player in a lobby has readied up.
+        Player player = new Player();
+        player.setName("Thomas");
+        String lobbyCode = gameService.createGameLobby(player);
+        gameService.readyUp(player, lobbyCode);
+        Assertions.assertTrue(gameRepository.getGameLobbyList().get(lobbyCode).getPlayerList().get(0).isReady());
     }
 }
---
+++ src/test/java/kea/dilemmaspilbackend/model/GameLobbyTest.java
@@ -0,0 +1,20 @@
+package kea.dilemmaspilbackend.model;
+
+import kea.dilemmaspilbackend.game.model.GameLobby;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameLobbyTest {
+    GameLobby gameLobby;
+
+    @BeforeEach // kaldes før hver @Test
+    public void setUp()  {
+        gameLobby = new GameLobby();
+    }
+
+    @Test
+    public void testCreateLobbyCode(){
+        Assertions.assertEquals(gameLobby.getLobbyCode().length(), 10);
+    }
+}
