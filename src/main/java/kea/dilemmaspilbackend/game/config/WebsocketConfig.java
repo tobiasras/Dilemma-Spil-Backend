5b405af0a46adf8af38d60f16fc90b4dca5062bb
tobiasras <tobiasras.job@gmail.com>
2022-11-23 10:15:40 +0100

    init

---
+++ src/main/java/kea/dilemmaspilbackend/game/config/WebsocketConfig.java
@@ -0,0 +1,26 @@
+package kea.dilemmaspilbackend.game.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/game/{lobby}").setAllowedOrigins("http://localhost:63342/").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
+
+
