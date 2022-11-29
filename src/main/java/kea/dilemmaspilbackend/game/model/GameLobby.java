4f5a47fc586c217cb1db805343c77f181c3072fc
danish <arjacokaki@hotmail.com>
2022-11-29 10:02:53 +0100

    Wrote unit tests

--- src/main/java/kea/dilemmaspilbackend/game/model/GameLobby.java
+++ src/main/java/kea/dilemmaspilbackend/game/model/GameLobby.java
@@ -22,7 +22,7 @@ public class GameLobby {
         // Code found at https://www.baeldung.com/java-random-string
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
+        int targetStringLength = 5;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
