16f6c8f0f6a84137c2b2b167a663935f20fd9e78
danish <arjacokaki@hotmail.com>
2022-11-29 14:57:15 +0100

    Added password hashing and jwt authentication to endpoint requests.

--- src/main/java/kea/dilemmaspilbackend/admin/controller/AdminController.java
+++ src/main/java/kea/dilemmaspilbackend/admin/controller/AdminController.java
@@ -1,22 +1,30 @@
 package kea.dilemmaspilbackend.admin.controller;
 
 import kea.dilemmaspilbackend.admin.model.AdminUser;
+import kea.dilemmaspilbackend.admin.security.JwtResponse;
 import kea.dilemmaspilbackend.admin.service.AdminUserService;
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@CrossOrigin
 @AllArgsConstructor
 public class AdminController {
     private AdminUserService adminUserService;
 
     @GetMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody AdminUser adminUser) {
-        adminUserService.login(adminUser);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<JwtResponse> login(@RequestBody AdminUser adminUser) throws Exception {
+        return new ResponseEntity<>(adminUserService.login(adminUser), HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity sayhi() {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/kea/dilemmaspilbackend/admin/repository/AdminUserRepository.java
+++ src/main/java/kea/dilemmaspilbackend/admin/repository/AdminUserRepository.java
@@ -1,4 +1,10 @@
 package kea.dilemmaspilbackend.admin.repository;
 
-public interface AdminUserRepository {
+import kea.dilemmaspilbackend.admin.model.AdminUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
+    Optional<AdminUser> findAdminUserByUsername(String username);
 }
