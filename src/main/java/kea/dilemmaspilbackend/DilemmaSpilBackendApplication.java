15695ba1d0b317d449f434902424993731d6521e
Danish-Pirate <arja0015@stud.kea.dk>
2022-12-14 14:49:54 +0100

    Removed unused imports

--- src/main/java/kea/dilemmaspilbackend/DilemmaSpilBackendApplication.java
+++ src/main/java/kea/dilemmaspilbackend/DilemmaSpilBackendApplication.java
@@ -3,8 +3,6 @@ package kea.dilemmaspilbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
